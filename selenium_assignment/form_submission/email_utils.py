11131a68e9826a17f70e21cf5199995498b3fcba
sakshi sata <138892477+sakshisata@users.noreply.github.com>
2024-07-14 17:02:33 +0530

    email_utils.py

--- selenium_assignment/form_submission/email_utils.py
+++ selenium_assignment/form_submission/email_utils.py
@@ -3,6 +3,6 @@
 from django.core.mail import EmailMessage
 
 def send_email_with_attachment(email_subject, to_email, cc_email, attachment_path):
-    email = EmailMessage(subject=email_subject, body='https://github.com/sakshisata/Selenium-webdriver Please find attached the file In this above github i had uploaded the rerquired source code and all the documentation ', to=[to_email], cc=[cc_email])
+    email = EmailMessage(subject=email_subject, body='https://github.com/sakshisata/Selenium-webdriver Please find attached the file. In this above github link i had uploaded all the required source code and all the documentation ', to=[to_email], cc=[cc_email])
     email.attach_file(attachment_path)
     email.send()
